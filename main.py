bf394e88c78177f62203934aa843356c716371ce
amytnikcs <adammytnik@student.agh.edu.pl>
2022-11-19 18:31:32 +0100

    commit 2

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import pygame,pymunk
 from shapes_collection import *
-
+import typing
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 space = pymunk.Space()
 GRAVITY = 200
-space.gravity = (0, GRAVITY)
+#space.gravity = (0, GRAVITY)
 pygame.display.set_caption("Second Game!")
 
 WHITE = (255, 255, 255)
@@ -41,6 +41,8 @@ def main():
 
     placing = False
     balls = []
+    dummy_ball = None
+    adding = False
     while run:
         pygame.display.update()
 
@@ -49,18 +51,33 @@ def main():
         WIN.fill(WHITE)
         for b in balls:
             b.draw(WIN, BLACK)
+        if(adding):
+            pos = pygame.mouse.get_pos()
+            x, y = pos
+            dummy_ball = Ball(x, y)
+            dummy_ball.add_to_space(space)
+            dummy_ball.shape.collision_type = 3
+            dummy_ball.shape.density = 0
+            dummy_ball.draw(WIN, RED)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
-            if event.type == pygame.MOUSEBUTTONUP:
+            if event.type == pygame.MOUSEBUTTONUP  and event.button == 1:
                 pos = pygame.mouse.get_pos()
-                print(pos)
                 x, y = pos
                 new_ball = Ball(x, y)
                 new_ball.add_to_space(space)
                 new_ball.draw(WIN, BLACK)
                 balls.append(new_ball)
+
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
+                adding = True
+
+            elif event.type == pygame.MOUSEBUTTONUP and event.button == 3:
+                adding = False
+
+
 
 
     pygame.quit()
