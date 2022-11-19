925eaa99c94ad039e0febdf46bc29e81efa0405a
Nepommuck <pawel.konopka5@gmail.com>
2022-11-19 14:34:29 +0100

    dodawanie kwadracików

--- main.py
+++ main.py
@@ -1,16 +1,61 @@
-# This is a sample Python script.
+import pygame
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+WIDTH, HEIGHT = 900, 500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+pygame.display.set_caption("Second Game!")
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+
+FPS = 60
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, giraffes')  # Press Ctrl+F8 to toggle the breakpoint.
+def add_block(cords):
+    x, y = cords
+    rect = pygame.Rect(x, y, 20, 20)
+    pygame.draw.rect(WIN, BLACK, rect)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def rect_clicked(click, rect):
+    x, y = click
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    return rect.x <= x <= rect.x + rect.width and rect.y <= y <= rect.y + rect.height
+
+
+def main():
+    clock = pygame.time.Clock()
+    run = True
+
+
+    # WIN.blit(SPACE, (0, 0))
+
+    WIN.fill(WHITE)
+    RECT = pygame.Rect(300, 0, 100, 50)
+    pygame.draw.rect(WIN, RED, RECT)
+
+    placing = False
+
+    while run:
+        pygame.display.update()
+
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+            if event.type == pygame.MOUSEBUTTONUP:
+                pos = pygame.mouse.get_pos()
+                print(pos)
+                add_block(pos)
+
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
