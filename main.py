cb0ab73621e324bf06db81e5666ed026cfee5f26
amytnikcs <adammytnik@student.agh.edu.pl>
2022-11-19 16:49:32 +0100

    commit init

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import pygame
-import os
+import pygame,pymunk
+from shapes_collection import *
 
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-
+space = pymunk.Space()
+GRAVITY = 200
+space.gravity = (0, GRAVITY)
 pygame.display.set_caption("Second Game!")
 
 WHITE = (255, 255, 255)
@@ -13,36 +15,40 @@ YELLOW = (255, 255, 0)
 
 FPS = 60
 
+
 def add_block(cords):
     x, y = cords
     rect = pygame.Rect(x, y, 20, 20)
     pygame.draw.rect(WIN, BLACK, rect)
-
 
 def rect_clicked(click, rect):
     x, y = click
 
     return rect.x <= x <= rect.x + rect.width and rect.y <= y <= rect.y + rect.height
 
+class wall():
+    pass
 
 def main():
     clock = pygame.time.Clock()
     run = True
 
-
     # WIN.blit(SPACE, (0, 0))
 
     WIN.fill(WHITE)
-    RECT = pygame.Rect(300, 0, 100, 50)
-    pygame.draw.rect(WIN, RED, RECT)
+    #RECT = pygame.Rect(300, 0, 100, 50)
+    #pygame.draw.rect(WIN, RED, RECT)
 
     placing = False
-
+    balls = []
     while run:
         pygame.display.update()
 
         clock.tick(FPS)
-
+        space.step(1/FPS)
+        WIN.fill(WHITE)
+        for b in balls:
+            b.draw(WIN, BLACK)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -50,7 +56,11 @@ def main():
             if event.type == pygame.MOUSEBUTTONUP:
                 pos = pygame.mouse.get_pos()
                 print(pos)
-                add_block(pos)
+                x, y = pos
+                new_ball = ball(x, y)
+                new_ball.add_to_space(space)
+                new_ball.draw(WIN, BLACK)
+                balls.append(new_ball)
 
 
     pygame.quit()
---
+++ shapes_collection.py
@@ -0,0 +1,22 @@
+from collision_for_body import *
+import pygame,pymunk
+
+
+class ball(collision_for_body):
+    def __init__(self, x, y):
+        collision_for_body.__init__(self, x, y)
+        self.add_velocity(0, 0)
+        self.body.body_type = pymunk.Body.DYNAMIC
+        self.setshape("ball")
+        self.shape.density = 1
+        self.shape.elasticity = 1
+        self.shape.collision_type = 1
+
+    def add_to_space(self, space):
+        space.add(self.body, self.shape)
+
+    def draw(self, WINDOW, COLOR):
+        x, y = self.body.position
+        pygame.draw.circle(WINDOW, COLOR, self.body.position, 8) # problemy do rozw ale odpalic trzeba
+class wall(collision_for_body):
+    def __init__(self, left_corner, top_corner):
