2723cfa8feaa40f13294329298c9f08dc726e40b
Nepommuck <pawel.konopka5@gmail.com>
2022-11-19 22:24:04 +0100

    camera works

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-import pygame,pymunk
-from shapes_collection import *
-from ground import Ground
-from Camera import Camera
+import random
+import pygame
+import pymunk as pm
 
+from Camera import Camera
+from Catapult import Catapult
+from Circle import Circle
+from ground import Ground
 
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-space = pymunk.Space()
-GRAVITY = 200
-space.gravity = (0, GRAVITY)
+
 pygame.display.set_caption("Second Game!")
 
 WHITE = (255, 255, 255)
@@ -19,69 +20,55 @@ YELLOW = (255, 255, 0)
 FPS = 60
 
 
-def add_block(cords):
-    x, y = cords
-    rect = pygame.Rect(x, y, 20, 20)
-    pygame.draw.rect(WIN, BLACK, rect)
+# def add_block(cords, space, squares):
+#     mass = 10
+#     radius = 25
+#     inertia = pm.moment_for_circle(mass, 0, radius, (0, 0))
+#     body = pm.Body(mass, inertia)
+#     x = cords[0]
+#     body.position = x, cords[1]
+#     shape = pm.Circle(body, radius, (0, 0))
+#     space.add(body, shape)
+#     squares.append(shape)
+
 
 def rect_clicked(click, rect):
     x, y = click
 
     return rect.x <= x <= rect.x + rect.width and rect.y <= y <= rect.y + rect.height
 
-class wall():
-    pass
-
-# class Point:
-#     def __init__(self):
-
-VEL = 5
-def pink_handle_movement(keys_pressed, red, ball):
-    if keys_pressed[pygame.K_LEFT]:  # LEFT
-        red.x -= VEL
-    if keys_pressed[pygame.K_RIGHT] and red.x + VEL + red.width < WIDTH:  # RIGHT
-        red.x += VEL
-    if keys_pressed[pygame.K_UP] and red.y - VEL > 0:  # UP
-        red.y -= VEL
-    if keys_pressed[pygame.K_DOWN] and red.y + VEL + red.height < HEIGHT - 15:  # DOWN
-        red.y += VEL
-
-
-def add_ball(pos, balls):
-    new_ball = Ball(pos[0], pos[1])
-    new_ball.add_to_space(space)
-    new_ball.draw(WIN, BLACK)
-    balls.append(new_ball)
-
 
 def main():
     clock = pygame.time.Clock()
     run = True
 
+    space = pm.Space()
+    space.gravity = (0.0, 900.0)
+    ch = space.add_collision_handler(0, 0)
+    ch.data["surface"] = WIN
+
+    camera = Camera((WIDTH, HEIGHT), (0, 0))
+
+    drawables = []
+
     # WIN.blit(SPACE, (0, 0))
 
     WIN.fill(WHITE)
-    #RECT = pygame.Rect(300, 0, 100, 50)
-    #pygame.draw.rect(WIN, RED, RECT)
+    RECT = pygame.Rect(300, 0, 100, 50)
+    pygame.draw.rect(WIN, RED, RECT)
+
+    catapult = Catapult(space,drawables,WIN,camera)
+    drawables.append(catapult)
 
     placing = False
-    balls = [Ball(100, 100)]
-    ground = Ground(WIN, space)
-    add_ball((), balls)
 
-    camera = Camera((WIDTH, HEIGHT))
-    camera.follow(balls[0])
+    ground = Ground(WIN, space, camera)
+    drawables.append(ground)
 
     while run:
         pygame.display.update()
 
         clock.tick(FPS)
-        space.step(1/FPS)
-        WIN.fill(WHITE)
-
-        for b in balls:
-            b.draw(WIN, BLACK)
-        ground.draw()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -90,7 +77,37 @@ def main():
             if event.type == pygame.MOUSEBUTTONUP:
                 pos = pygame.mouse.get_pos()
                 print(pos)
-                add_ball(pos, balls)
+                circle = Circle(WIN, camera, pos)
+                space.add(circle.shape, circle.body)
+                drawables.append(circle)
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                projectile = catapult.space_clicked()
+                if projectile is not None:
+                    space.add(projectile.body, projectile.shape)
+                    camera.follow(projectile)
+
+        WIN.fill(pygame.Color("white"))
+
+        to_remove = []
+        for drawable in drawables:
+            # if drawable.get_pos()[1] > 400:
+            #     to_remove.append(drawable)
+            p = tuple(map(int, drawable.get_pos()))
+            drawable.draw()
+
+        for ball in to_remove:
+            space.remove(ball.shape, ball.body)
+            drawables.remove(ball)
+
+        ### Update physics
+        dt = 1.0 / 60.0
+        for x in range(1):
+            space.step(dt)
+
+        ### Flip screen
+        pygame.display.flip()
+        clock.tick(50)
+        pygame.display.set_caption("fps: " + str(clock.get_fps()))
 
     pygame.quit()
 
