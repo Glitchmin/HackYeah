29b595aa47398eac99c3eb0da63773df5b6fe459
Nepommuck <pawel.konopka5@gmail.com>
2022-11-19 18:24:21 +0100

    ground

---
+++ ground.py
@@ -0,0 +1,28 @@
+import pygame, pymunk
+
+GREEN = (0, 255, 0)
+
+
+class Ground:
+    def __init__(self, window, space, height=50):
+        self.window = window
+        self.space = space
+        self.height = height
+        self.width = window.get_width()
+        self.color = GREEN
+
+        self.pos = (0, self.window.get_height() - self.height)
+
+    def draw(self):
+        body = pymunk.Body(body_type=pymunk.Body.STATIC)
+        w, h = self.width, self.height
+
+        body.position = self.pos[0] + w / 2, self.pos[1] + h / 2
+
+        rect = pygame.Rect(self.pos[0], self.pos[1], self.width, self.height)
+        pygame.draw.rect(self.window, GREEN, rect)
+
+        vs = [(-w / 2, -h / 2), (w / 2, -h / 2), (w / 2, h / 2), (-w / 2, h / 2)]
+        shape = pymunk.Poly(body, vs)
+
+        self.space.add(body, shape)
