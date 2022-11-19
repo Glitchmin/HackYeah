e65bfc6876a62fe2eaa7777a05cfa0c33743ef13
Adam <adam.p.kulczycki@gmail.com>
2022-11-19 18:39:19 +0100

    catapatutl prototype

---
+++ Catapult.py
@@ -0,0 +1,44 @@
+import math
+
+import pygame
+from pymunk import Space
+
+import collisionforbody
+from shapes_collection import Ball
+
+
+class Catapult:
+
+    def __init__(self, space: Space, display: pygame.display, balls: [collisionforbody]):
+        self.base_pos = 200, 750-200
+        self.mass = 100
+        self.angle = -45
+        self.length = 200
+        self.end_point = 0, 0
+        self.space = space
+        self.angular_speed = 0.0
+        self.calc_end()
+        self.display = display
+        self.yeet_force = 500
+        self.is_spinning = False
+        self.balls = balls
+
+    def space_clicked(self):
+        if not self.is_spinning:
+            self.angular_speed = -7
+            self.is_spinning = True
+            return
+        self.angular_speed = 0
+        self.is_spinning = False
+        ball = Ball(self.end_point[0], self.end_point[1])
+        ball.add_to_space(self.space)
+        ball.add_velocity(self.yeet_force * math.sin(math.radians(self.angle - 90)), self.yeet_force * math.cos(math.radians(self.angle - 90)))
+        self.balls.append(ball)
+
+    def calc_end(self):
+        self.angle += self.angular_speed
+        diff = self.length * math.sin(math.radians(self.angle)), self.length * math.cos(math.radians(self.angle))
+        self.end_point = self.base_pos[0] + diff[0], self.base_pos[1] + diff[1]
+
+    def draw(self):
+        pygame.draw.line(self.display, pygame.Color("brown"), self.base_pos, self.end_point, 5)
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import pygame,pymunk
+import pygame, pymunk
+
+from Catapult import Catapult
 from shapes_collection import *
 
-WIDTH, HEIGHT = 900, 500
+WIDTH, HEIGHT = 1200, 750
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 space = pymunk.Space()
 GRAVITY = 200
@@ -21,13 +23,16 @@ def add_block(cords):
     rect = pygame.Rect(x, y, 20, 20)
     pygame.draw.rect(WIN, BLACK, rect)
 
+
 def rect_clicked(click, rect):
     x, y = click
 
     return rect.x <= x <= rect.x + rect.width and rect.y <= y <= rect.y + rect.height
 
+
 class wall():
     pass
+
 
 def main():
     clock = pygame.time.Clock()
@@ -36,16 +41,17 @@ def main():
     # WIN.blit(SPACE, (0, 0))
 
     WIN.fill(WHITE)
-    #RECT = pygame.Rect(300, 0, 100, 50)
-    #pygame.draw.rect(WIN, RED, RECT)
+    # RECT = pygame.Rect(300, 0, 100, 50)
+    # pygame.draw.rect(WIN, RED, RECT)
 
     placing = False
     balls = []
+    catapult = Catapult(space, WIN,balls)
     while run:
         pygame.display.update()
 
         clock.tick(FPS)
-        space.step(1/FPS)
+        space.step(1 / FPS)
         WIN.fill(WHITE)
         for b in balls:
             b.draw(WIN, BLACK)
@@ -61,7 +67,11 @@ def main():
                 new_ball.add_to_space(space)
                 new_ball.draw(WIN, BLACK)
                 balls.append(new_ball)
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                catapult.space_clicked()
 
+        catapult.calc_end()
+        catapult.draw()
 
     pygame.quit()
 
