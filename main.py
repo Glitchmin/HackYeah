527f1b88096e8190ac5e404e6c99f501932b7d01
Adam <adam.p.kulczycki@gmail.com>
2022-11-19 21:58:47 +0100

    main.py code cleanup, screen size adjusted

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import random
 import pygame
+import GameStates
 import pymunk as pm
 
 from Camera import Camera
 from Catapult import Catapult
 from Circle import Circle
+import ctypes
 
-WIDTH, HEIGHT = 900, 500
+user32 = ctypes.windll.user32
+WIDTH, HEIGHT = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
-pygame.display.set_caption("Second Game!")
+pygame.display.set_caption("Camelopard Castle: Siege")
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -17,6 +20,7 @@ RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 
 FPS = 60
+
 
 #
 # def add_block(cords, space, squares):
@@ -35,6 +39,45 @@ def rect_clicked(click, rect):
     x, y = click
 
     return rect.x <= x <= rect.x + rect.width and rect.y <= y <= rect.y + rect.height
+
+
+def calculate_physics(drawables, space):
+    to_remove = []
+    for drawable in drawables:
+        # if drawable.get_pos()[1] > 400:
+        #     to_remove.append(drawable)
+        p = tuple(map(int, drawable.get_pos()))
+        drawable.draw()
+    for ball in to_remove:
+        space.remove(ball.shape, ball.body)
+        drawables.remove(ball)
+
+    dt = 1.0 / 60.0
+    for x in range(1):
+        space.step(dt)
+
+
+def update_screen(clock):
+    pygame.display.update()
+    WIN.fill(pygame.Color("white"))
+    clock.tick(FPS)
+    pygame.display.flip()
+
+def handle_input(camera, catapult, drawables, run, space):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            pygame.quit()
+        if event.type == pygame.MOUSEBUTTONUP:
+            pos = pygame.mouse.get_pos()
+            print(pos)
+            circle = Circle(WIN, camera, pos)
+            space.add(circle.shape, circle.body)
+            drawables.append(circle)
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            projectile = catapult.space_clicked()
+            if projectile is not None:
+                space.add(projectile.body, projectile.shape)
 
 
 def main():
@@ -56,54 +99,18 @@ def main():
     RECT = pygame.Rect(300, 0, 100, 50)
     pygame.draw.rect(WIN, RED, RECT)
 
-    catapult = Catapult(space,drawables,WIN,camera)
+    catapult = Catapult(space, drawables, WIN, camera)
     drawables.append(catapult)
-
-    placing = False
+    current_state = GameStates.PL_1_BUILDING
 
     while run:
-        pygame.display.update()
 
-        clock.tick(FPS)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-                pygame.quit()
-            if event.type == pygame.MOUSEBUTTONUP:
-                pos = pygame.mouse.get_pos()
-                print(pos)
-                circle = Circle(WIN, camera, pos)
-                space.add(circle.shape, circle.body)
-                drawables.append(circle)
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                projectile = catapult.space_clicked()
-                if projectile is not None:
-                    space.add(projectile.body, projectile.shape)
-        WIN.fill(pygame.Color("white"))
-
-        to_remove = []
-        for drawable in drawables:
-            # if drawable.get_pos()[1] > 400:
-            #     to_remove.append(drawable)
-            p = tuple(map(int, drawable.get_pos()))
-            drawable.draw()
-
-        for ball in to_remove:
-            space.remove(ball.shape, ball.body)
-            drawables.remove(ball)
-
-        ### Update physics
-        dt = 1.0 / 60.0
-        for x in range(1):
-            space.step(dt)
-
-        ### Flip screen
-        pygame.display.flip()
-        clock.tick(50)
-        pygame.display.set_caption("fps: " + str(clock.get_fps()))
+        handle_input(camera, catapult, drawables, run, space)
+        calculate_physics(drawables, space)
+        update_screen(clock)
 
     pygame.quit()
+
 
 
 if __name__ == "__main__":
