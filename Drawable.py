65d4293f121ce15055922f7a7df64e05bb044ece
jchyczynski <jasiek.ch16@gmail.com>
2022-11-19 19:00:49 +0100

    Drawable update, Physical added

--- Drawable.py
+++ Drawable.py
@@ -7,11 +7,13 @@ from Camera import Camera
 class Drawable:
 
     def __init__(self, window, camera: Camera):
-        pass
+        self.window = window
+        self.camera = camera
 
     @abstractmethod
     def get_pos(self) -> Tuple[float, float]:
         pass
 
+    @abstractmethod
     def draw(self):
         pass
