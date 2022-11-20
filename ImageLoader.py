1e3f553ac1ddb89fc08adf8c5a5052ae3976e579
amytnikcs <adammytnik@student.agh.edu.pl>
2022-11-20 08:56:26 +0100

    more textures added :)

--- ImageLoader.py
+++ ImageLoader.py
@@ -9,6 +9,18 @@ class ImageLoader:
         self.images["testgrass.png"].convert()
         self.images["szary.png"] = pygame.image.load("szary.png")
         self.images["szary.png"].convert()
+        self.images["giraffe.png"] = pygame.image.load("giraffe.png")
+        self.images["giraffe.png"].convert()
+        self.images["giraffe_head.png"] = pygame.image.load("giraffe_head.png")
+        self.images["giraffe_head.png"].convert()
+        self.images["full_giraffe.png"] = pygame.image.load("full_giraffe.png")
+        self.images["full_giraffe.png"].convert()
+        self.images["full_loong_giraffe.png"] = pygame.image.load("full_loong_giraffe.png")
+        self.images["full_loong_giraffe.png"].convert()
+        self.images["full_loong_giraffe_head.png"] = pygame.image.load("full_loong_giraffe_head.png")
+        self.images["full_loong_giraffe_head.png"].convert()
+        self.images["full_loong_giraffe_body.png"] = pygame.image.load("full_loong_giraffe_body.png")
+        self.images["full_loong_giraffe_body.png"].convert()
 
     def draw_on_pos(self, pos: Tuple[float, float], size: Tuple[float, float], image_name, display):
         display.blit(pygame.transform.scale(self.images[image_name], size), pygame.Rect(*pos, *size))
--- TextureRectangle.py
+++ TextureRectangle.py
@@ -8,13 +8,13 @@ from Physical import Physical
 
 
 class TextureRectangle(Physical):
-    def __init__(self,shape, display, image_name: str,image_loader: ImageLoader, camera: Camera, pos: Tuple[float, float], size: Tuple[float, float]):
+    def __init__(self,shape, body, display, image_name: str,image_loader: ImageLoader, camera: Camera, pos: Tuple[float, float], size: Tuple[float, float]):
         self.display = display
         self.size = size
         self.image_loader = image_loader
         self.image_name = image_name
         self.pos = pos
-        super().__init__(shape, display, camera)
+        super().__init__(shape, body, display, camera)
 
     def get_size(self) -> Tuple[float, float]:
         return self.size
